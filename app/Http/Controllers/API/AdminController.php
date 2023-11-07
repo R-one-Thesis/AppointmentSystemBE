7bae050499564135af3156e1d2392259c8e6ab78
Jim Lao <laojimmarklino@gmail.com>
2023-11-07 20:30:16 +0800

    Update AdminController.php

--- app/Http/Controllers/API/AdminController.php
+++ app/Http/Controllers/API/AdminController.php
@@ -8,6 +8,7 @@ use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
