75b73c92d89f5034cb44c5a288341e9f5f8a7bab
Jim Lao <laojimmarklino@gmail.com>
2023-11-08 22:24:36 +0800

    Update AuthController.php

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -44,7 +44,7 @@ class AuthController extends Controller
                 'token' => $token,
                 'user_name' => $fullName,
                 'user_type' => $user->user_type,
-                'admin' => $adminID
+                'admin_id' => $adminID
                 
             ]);
         }
