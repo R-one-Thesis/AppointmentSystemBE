6fe147a7298405466745e2836b7629ec5302ec5f
Jim Lao <laojimmarklino@gmail.com>
2023-12-06 23:31:00 +0800

    Update BookingController.php

--- app/Http/Controllers/API/BookingController.php
+++ app/Http/Controllers/API/BookingController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\Booking;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class BookingController extends Controller
 {
